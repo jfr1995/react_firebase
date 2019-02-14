78d9abf1b72d941e67ca14f0a7f07a3fb612ef13
Juan Ramirez <juanramirez1776@gmail.com>
2019-02-14 01:46:19 -0800

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,22 @@
+// App/index.js
+
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import Navigation from '../Navigation'
+
+
+
+const App = () => (
+
+<div>
+    <Router>
+        <Navigation/>
+    </Router>
+</div>
+
+);
+
+export default App;
+
+
